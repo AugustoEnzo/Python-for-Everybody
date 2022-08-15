f73a0c9f6111f29c7aed0fad13cae3a672e3c316
Enzo Augusto <enzoaugustolima2019@gmail.com>
2022-08-14 23:38:31 -0400

    v08 - Week tree of Course four Completed (Course Three Completed).

--- SQLite/Scripts/SQL1.sql
+++ SQLite/Scripts/SQL1.sql
@@ -17,3 +17,12 @@ INSERT INTO Track (title, rating, len, count, album_id, genre_id) VALUES ('Black
 INSERT INTO Track (title, rating, len, count, album_id, genre_id) VALUES ('Stairway', 5, 482, 0, 2, 1);
 INSERT INTO Track (title, rating, len, count, album_id, genre_id) VALUES ('About to Rock', 5, 313, 0, 1, 2);
 INSERT INTO Track (title, rating, len, count, album_id, genre_id) VALUES ('Who Made Who', 5, 207, 0, 1, 2);
+
+SELECT Album.title, Artist.name FROM Album JOIN Artist ON Album.artist_id = Artist.id
+
+SELECT Track.title, Genre.name FROM Track JOIN Genre ON Track.genre_id = Genre.id
+
+SELECT Track.title, Track.genre_id, Genre.id, Genre.name FROM Track JOIN Genre
+
+SELECT Track.title, Artist.name, Album.title, Genre.name FROM Track JOIN Genre JOIN Album JOIN Artist ON Track.genre_id = Genre.id AND
+Track.album_id = Album.id AND Album.artist_id = Artist.id
